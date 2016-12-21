7efeddee6c01887d7e390578f5dc1413a148ff20
Sarah Basinger <sarrey@gmail.com>
2016-12-21 10:13:45 -0500

    git init

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+
+	console.log("test");
+
+	// The base URL for all api calls
+	var apiBaseUrl = "http://api.themoviedb.org/3/";
+
+	// Base image URL
+	var imageBaseUrl = "http://image.thdb.org/t/p/";
+
+	const nowPlayingUrl = apiBaseUrl + "movie/now_playing?api_key=" + apiKey;
+
+
+})
